8ec15658e781214b731cbe4a586b2449e40681b8
tiramisa <miller19988@gmail.com>
2023-07-18 18:14:44 +0100

    slider

---
+++ src/index.js
@@ -0,0 +1,17 @@
+let offset = 0;
+const sliderLine = document.querySelector(".slider-line");
+document.querySelector(".slider-next").addEventListener("click", function () {
+  offset = offset + 256;
+  if (offset > 1536) {
+    offset = 0;
+  }
+  sliderLine.style.left = -offset + "px";
+});
+
+document.querySelector(".slider-prev").addEventListener("click", function () {
+  offset = offset - 256;
+  if (offset < 0) {
+    offset = 1536;
+  }
+  sliderLine.style.left = -offset + "px";
+});
